bb35104e24d98057586fca47c63e0cfc3ef388b3
Himanshu Mishra <himanshu2014iit@gmail.com>
2017-01-27 17:48:55 +0530

    Create home page

---
+++ app.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+from flask import Flask, render_template, redirect, request
+from flask_session import Session
+
+app = Flask(__name__, static_url_path='/static')
+sess = Session()
+
+
+@app.route("/")
+def main():
+    return render_template('home/index.html')
+
+
+@app.route("/abwid")
+def abwid():
+    return render_template('home/abwid.html')
+
+
+@app.route("/contact")
+def contact():
+    return render_template('home/contact.html')
+
+
+@app.route("/projects")
+def projects():
+    return render_template('home/projects.html')
+
+
+
+app.secret_key = os.environ["APP_SECRET_KEY"]
+app.config['SESSION_TYPE'] = 'filesystem'
+
+sess.init_app(app)
+app.debug = False
+
+if __name__ == '__main__':
+    app.run(debug=True)
