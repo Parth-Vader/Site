64195201bc1a7ade4bf158ef85fc11fc05bdb51e
Himanshu Mishra <himanshu2014iit@gmail.com>
2017-03-06 17:58:21 +0530

    Do not update with a response post

--- handlers/blog_updater.py
+++ handlers/blog_updater.py
@@ -25,7 +25,12 @@ with open('/home/hunter/orkohunter.net/database/blog/last_id', 'r') as f:
 
 new_post = atom['entries'][0]
 
-if not last_id == new_post['id']:
+is_old_post = last_id == new_post['id']
+is_not_a_reply = 'tag' in new_post  # Responses don't have tags
+
+if not is_old_post and is_not_a_reply:
+    print("I'm in")
+    print(new_post['title'])
     last_id = new_post['id']
     with open('/home/hunter/orkohunter.net/database/blog/last_id', 'w') as f:
         f.write(last_id)
